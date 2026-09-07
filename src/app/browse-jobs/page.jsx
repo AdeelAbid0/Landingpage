e3d3807d99c1649442cbc96de9de96ea3d81c914
Waqas106 <m.waqas987321@gmail.com>
2026-09-07 16:37:07 +0500

    Added browse jobs page

--- src/app/browse-jobs/page.jsx
+++ src/app/browse-jobs/page.jsx
@@ -1,3 +1,5 @@
+import SearchBar from "@/shared/components/SearchBar";
+
 export const metadata = {
   title: "Browse Jobs",
   description:
@@ -9,8 +11,16 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div>
-      <h1>Browse Jobs</h1>
+    <div className="relative flex items-center justify-center min-h-full overflow-hidden">
+      <div className="absolute -top-40 left-1/2 -translate-x-1/2 w-[600px] h-[300px] bg-gradient-to-br from-purple-400 via-purple-300 to-blue-300 opacity-40 blur-[100px] rounded-full pointer-events-none" />
+      <div className="absolute -bottom-10 left-1/2 -translate-x-1/2 w-[600px] h-[150px] bg-gradient-to-br from-purple-800 to-purple-500 opacity-40 blur-[100px] rounded-full pointer-events-none" />
+      <div className="" />
+      <div className="flex flex-col gap-6 w-full max-w-[836px]">
+        <h2 className="text-[32px] font-semibold leading-100% text-center">
+          Browse jobs
+        </h2>
+        <SearchBar />
+      </div>
     </div>
   );
 }
